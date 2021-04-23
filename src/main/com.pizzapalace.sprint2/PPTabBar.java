1c1d83eb5e87a15d1c1d884b5d18f14319c7ea67
Turner Eison <turner@turnereison.com>
2021-04-23 12:54:57 -0400

    Tab bar looking good. No implementation of the buttons just yet.

---
+++ src/main/com.pizzapalace.sprint2/PPTabBar.java
@@ -0,0 +1,31 @@
+package com.pizzapalace.sprint2;
+
+public class PPTabBar {
+    public void onHomeButtonPressed() {
+        System.out.println("Navigate home");
+    }
+
+    public void onAboutUsPressed() {
+        System.out.println("navigate to about us");
+    }
+
+    public void onMenuPressed() {
+        System.out.println("Navigate to menu");
+    }
+
+    public void onDIYPressed() {
+        System.out.println("Navigate to DIY");
+    }
+
+    public void onSpecialsPressed() {
+        System.out.println("Navigate to specials");
+    }
+
+    public void onCartPressed() {
+        System.out.println("Navigate to cart");
+    }
+
+    public void onLogInPressed() {
+        // TODO: Handle sign in versus navigating to account page
+    }
+}
